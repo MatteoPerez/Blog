76cd71312462c2ae0704dd4e706eae8ab0e07503
MatteoPerez <145006394+MatteoPerez@users.noreply.github.com>
2024-08-21 15:08:04 +0300

    Amélioration responsive CSS

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,33 @@ document.addEventListener('DOMContentLoaded', function(){
         this.setAttribute('aria-expended', 'false');
     })
 });
+
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function showDropdown() {
+  document.getElementById("dropdownContent").classList.toggle("show");
+}
+
+// Check which type of device the user is on
+window.onload = function() {
+    if (window.innerWidth < 768) {
+        document.getElementById('header__mobile').style.display = 'grid';
+        document.getElementById('header__desktop').style.display = 'none';
+    } else {
+        document.getElementById('header__desktop').style.display = 'grid';
+        document.getElementById('header__mobile').style.display = 'none';
+    }
+};
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown__content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
